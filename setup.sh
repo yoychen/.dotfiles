0ee636042c1a8655b7d88e18bc165746b05df1b0
yoychen <yui12327@gmail.com>
2023-09-04 00:34:09 +0800

    Update docker-compose version

--- setup.sh
+++ setup.sh
@@ -36,7 +36,7 @@ install_docker() {
 }
 
 install_docker_compose() {
-    sudo curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo curl -SL https://github.com/docker/compose/releases/download/v2.20.3/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
 }
 
